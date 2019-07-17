a24737a9ec21e47e19c1b756c46935d99d40aeb3
alparslancay <alparslancay1@gmail.com>
2019-07-17 22:35:22 +0300

    Sürüm 1 Eklendi

---
+++ SiparisOtomasyon/Yonetici.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiparişOtomasyon
+{
+    public class Yonetici :  Kisi
+    {
+
+        public int UrunAgirlik { get; set; }
+        public double UrunFiyat { get; set; }
+        public int UrunKodu { get; set; }
+        public int UrunStokAdet { get; set; }
+        public string Urunturu { get; set; }
+    }
+}
